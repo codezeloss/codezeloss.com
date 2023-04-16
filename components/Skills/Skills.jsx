5b36057b64177d3845d21dad20e50088646fd069
codezeloss <elhoussaineaitX@gmail.com>
2023-04-16 02:06:25 +0000

    Improving UI, dark mode...

--- components/Skills/Skills.jsx
+++ components/Skills/Skills.jsx
@@ -12,10 +12,10 @@ const techs = [
 
 const Skills = () => {
   return (
-    <section id="skills" className="mb-36 7bp:mt-20">
+    <section id="skills" className="mb-36 7bp:mt-28 7bp:mb-20">
       <h1 className="mb-20 6bp:mb-10">Skills</h1>
 
-      <div className="grid gap-x-24 gap-y-20 grid-cols-3 2bp:gap-x-16 2bp:gap-y-12 2bp:mb-24 6bp:grid-cols-2 7bp:gap-x-6 7bp:gap-y-6 7bp:mt-24">
+      <div className="grid gap-x-24 gap-y-20 grid-cols-3 2bp:gap-x-16 2bp:gap-y-12 2bp:mb-24 6bp:grid-cols-2 7bp:gap-x-6 7bp:gap-y-6">
         {techs.map((skill) => (
           <div>
             <h2>{skill}</h2>
